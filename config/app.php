1218c6f83c17cf16fd9fa77d320ba43903f71011
Nurbol <4502040@gmail.com>
2020-01-20 00:08:54 +0600

    env

--- config/app.php
+++ config/app.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'debug' => true,
+    'debug' => env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -41,7 +41,7 @@ return [
     |
     */
 
-    'url' => 'http://localhost',
+    'url' => env('APP_URL', 'http://localhost'),
 
     /*
     |--------------------------------------------------------------------------
@@ -101,7 +101,7 @@ return [
     |
     */
 
-    'key' => 'ae2Fd8pZfswSPALcVClWUL4Vuaaxe8d8',
+    'key' => env('APP_KEY', ''),
 
     'cipher' => 'AES-256-CBC',
 
